89d62f9ba68ad35a797ffe022a0bdaef33cd5ee2
amiller5233@gmail.com <amiller5233@gmail.com>
2023-07-12 20:26:33 -0700

    create xcode project

---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Adam Miller on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
